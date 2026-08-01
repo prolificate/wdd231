aeee9f3536911d59ce9dac5d8d5dfafab20d97ae
Eniola <eniolafolabi105@gmail.com>
2026-08-01 21:37:14 +0100

    commit

--- chamber/data/business.mjs
+++ chamber/data/business.mjs
@@ -9,41 +9,47 @@ export const business = [
         name: "Cadbury Nigeria Plc",
         images : "images/cadbury.webp",
         address : "Lagos Nigeria",
-        description : "One of Africa biggest conglomerates."
+        description : "One of Nigeria's famous chocolate drinks."
     },
      {
         name: "Nigeria Bottling Company Plc",
         images : "images/bottles.webp",
         address : "Lagos Nigeria",
-        description : "One of Africa biggest conglomerates."
+        description : "Famous for bottling drinls in Nigeria and in Africa."
     },
      {
         name: "Dangote Plc",
-        images : "images/dangote.webp",
+        images : "images/farm.webp",
         address : "Lagos Nigeria",
         description : "One of Africa biggest conglomerates."
     },
      {
         name: "Dangote Plc",
-        images : "images/dangote.webp",
+        images : "images/print.webp",
         address : "Lagos Nigeria",
         description : "One of Africa biggest conglomerates."
     },
      {
         name: "Dangote Plc",
-        images : "images/dangote.webp",
+        images : "images/tech.webp",
         address : "Lagos Nigeria",
         description : "One of Africa biggest conglomerates."
     },
      {
         name: "Dangote Plc",
-        images : "images/dangote.webp",
+        images : "images/Greenleaf-logo.webp",
         address : "Lagos Nigeria",
         description : "One of Africa biggest conglomerates."
     },
      {
         name: "Dangote Plc",
-        images : "images/dangote.webp",
+        images : "images/taste.webp",
+        address : "Lagos Nigeria",
+        description : "One of Africa biggest conglomerates."
+    },
+    {
+        name: "Dangote Plc",
+        images : "images/fashion.webp",
         address : "Lagos Nigeria",
         description : "One of Africa biggest conglomerates."
     },
--- chamber/scripts/dicover.js
+++ chamber/scripts/dicover.js
@@ -1,12 +1,21 @@
 import { business } from "../data/business.mjs";
 
-const grid = document.querySelector(".discover");
+const grid = document.querySelector(".business-section");
 
-grid.forEach(discover => {
+business.forEach(discover => {
     const info = document.createElement("article");
-    info.classList.add("info");
+    info.classList.add("business-card");
 
-    info.innerHTML='<img src = "${discover.images}" alt = "${discover.description}" loading= "lazy"> <div class= "infoContent"> <h2>${discover.name}</h2> <p><strong>Location:</strong> {discover.address}</p> <button>Learn More </button>';
+    info.innerHTML = `
+        <img src="${discover.images}" alt="${discover.description}" loading="lazy">
+        <div class="home">
+            <h3>${discover.name}</h3>
+            <p><strong>Location:</strong> ${discover.address}</p>
+            <p>${discover.description}</p>
+            <button>Learn More</button>
+        </div>
+    `;
 
     grid.appendChild(info);
 });
+
